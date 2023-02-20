adf2365af79548cacd7ce74446303abe4c1bec30
Satoru Hagiwara <hagiwara@fusic.co.jp>
2023-02-20 12:36:08 +0000

    eslint fix

--- front/src/components/board/common/sutehai.tsx
+++ front/src/components/board/common/sutehai.tsx
@@ -1,21 +1,17 @@
-import { SutehaiProp } from "../type";
-import style from './sutehai.module.css';
-import ms1_1 from '../hai/p_ms1_1.gif';
-import ms1_3 from '../hai/p_ms1_3.gif';
-import { getHaiSrc } from "../hai/hai_info";
+import type { SutehaiProp } from '../type'
+import style from './sutehai.module.css'
+import { getHaiSrc } from '../hai/hai_info'
 
-type SutehaiConstProp = {
-    sutehai: SutehaiProp[];
+interface SutehaiConstProp {
+  sutehai: SutehaiProp[]
 }
 
-export const Sutehai = ({sutehai}: SutehaiConstProp) => {
-    return <>
-        {sutehai.map((sutehaiPai) => {
-            return <div className={style.sutehai}>
-                {sutehaiPai.type === 'normal' ? <img src={getHaiSrc(sutehaiPai.hai, 1)} /> : <img src={getHaiSrc(sutehaiPai.hai, 3)} /> }
-            </div>
-        })}
-
-    </>;
-
+export const Sutehai = ({ sutehai }: SutehaiConstProp): JSX.Element => {
+  return <>
+    {sutehai.map((sutehaiPai) => {
+      return <div className={style.sutehai} key={sutehaiPai.hai} >
+        {sutehaiPai.type === 'normal' ? <img src={getHaiSrc(sutehaiPai.hai, 1)} /> : <img src={getHaiSrc(sutehaiPai.hai, 3)} /> }
+      </div>
+    })}
+  </>
 }
--- front/src/reportWebVitals.ts
+++ front/src/reportWebVitals.ts
@@ -1,15 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import type { ReportHandler } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  // eslint-disable-next-line
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // eslint-disable-next-line
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+      getCLS(onPerfEntry)
+      getFID(onPerfEntry)
+      getFCP(onPerfEntry)
+      getLCP(onPerfEntry)
+      getTTFB(onPerfEntry)
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
